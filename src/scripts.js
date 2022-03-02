129672ee88b9940e45af93366ebc850ec9373ad6
Jordan Skomal <jskomal@gmail.com>
2022-03-02 14:37:58 -0700

    Update Styling

--- src/scripts.js
+++ src/scripts.js
@@ -12,5 +12,6 @@ import './images/plane-clr.svg'
 import './images/earth-blk.svg'
 import './images/earth-wht.svg'
 import './images/island.svg'
+import './images/paper-plane-clr.svg'
 
 console.log('This is the JavaScript entry file - your code begins here.')
