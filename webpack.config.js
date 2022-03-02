ff2d8b6f702987dbaf6c84f5627c7a6173d63b14
Jordan Skomal <jskomal@gmail.com>
2022-03-02 10:12:38 -0700

    Add Required Files

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,22 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-  "mode": "none",
-  "entry": "./src/scripts.js",
-  "output": {
-    "path": __dirname + '/dist',
-    "filename": "bundle.js",
-    sourceMapFilename: "bundle.js.map"
+  mode: 'none',
+  entry: './src/scripts.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js',
+    sourceMapFilename: 'bundle.js.map'
   },
   devServer: {
     contentBase: path.join(__dirname, 'dist')
   },
-  "devtool": "source-map",
-  "module": {
-    "rules": [
+  devtool: 'source-map',
+  module: {
+    rules: [
       {
-        test: /\.s[ac]ss$/i,
-        use: [
-          "style-loader",
-          "css-loader",
-          "sass-loader",
-        ],
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
@@ -37,4 +33,4 @@ module.exports = {
       }
     ]
   }
-};
+}
