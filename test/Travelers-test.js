806b3519757f54e7865c408d6c025823d632b888
Jordan Skomal <jskomal@gmail.com>
2022-03-02 15:07:58 -0700

    Write test for Travelers

---
+++ test/Travelers-test.js
@@ -0,0 +1,33 @@
+import chai from 'chai'
+const expect = chai.expect
+import { testData } from './test-data'
+
+describe("Traveler's Tests", () => {
+  beforeEach(() => {
+    traveller1 = new Traveler(testData[0])
+    traveller2 = new Traveler(testData[1])
+    traveller3 = new Traveler(testData[2])
+  })
+
+  it('should be a function', () => {
+    expect(Traveler).to.be.a('function')
+  })
+
+  it('should be an instantiation of the Traveler Class', () => {
+    expect(traveller1).to.be.an.instanceof(Traveler)
+    expect(traveller2).to.be.an.instanceof(Traveler)
+    expect(traveller3).to.be.an.instanceof(Traveler)
+  })
+
+  it('should have an ID, name, and travellerType', () => {
+    expect(traveller1.id).to.equal(1)
+    expect(traveller1.name).to.equal('Jordan Skomal')
+    expect(traveller1.travellerType).to.equal('relaxer')
+  })
+
+  it("should have a method to return the user's first name", () => {
+    expect(traveller1.getFirstName()).to.equal('Jordan')
+    expect(traveller2.getFirstName()).to.equal('Taylor')
+    expect(traveller3.getFirstName()).to.equal('John')
+  })
+})
---
+++ test/test-data.js
@@ -0,0 +1,22 @@
+const travellerTestData = [
+  {
+    id: 1,
+    name: 'Jordan Skomal',
+    travelerType: 'relaxer'
+  },
+  {
+    id: 2,
+    name: 'Taylor Davis',
+    travelerType: 'thrill-seeker'
+  },
+  {
+    id: 3,
+    name: 'John Savino',
+    travelerType: 'foodie'
+  }
+]
+
+const destinationTestData = {}
+const tripsTestData = {}
+
+export { travellerTestData }
