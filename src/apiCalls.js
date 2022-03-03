5b5f3f435bd6a115cd9e77fb9a523d821a8dc903
Jordan Skomal <jskomal@gmail.com>
2022-03-03 13:20:45 -0700

    Remove Console logs

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,7 +1,6 @@
 const fetchData = (path) => {
   return fetch(`http://localhost:3001/api/v1/${path}`)
     .then((response) => response.json())
-    .then((data) => console.log(data))
     .catch((error) => console.log(error))
 }
 
