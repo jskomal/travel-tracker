2cf9778a840ece6a507ba000d80881dda3df20a0
Jordan Skomal <jskomal@gmail.com>
2022-03-03 13:14:52 -0700

    Fix fetch path

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,8 @@
 const fetchData = (path) => {
-  return fetch(`htt[://localhost:3001/api/v1/${path}]`)
+  return fetch(`http://localhost:3001/api/v1/${path}`)
     .then((response) => response.json())
+    .then((data) => console.log(data))
     .catch((error) => console.log(error))
 }
+
+export { fetchData }
