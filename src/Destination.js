b8923b7b09f70ca416eda88ac9f80c401827127a
Jordan Skomal <jskomal@gmail.com>
2022-03-03 12:56:26 -0700

    Implement Destination tests

--- src/Destination.js
+++ src/Destination.js
@@ -1,5 +1,12 @@
 class Destination {
-  constructor(destination) {}
+  constructor(destination) {
+    this.id = destination.id
+    this.destination = destination.destination
+    this.estimatedLodgingCostPerDay = destination.estimatedLodgingCostPerDay
+    this.estimatedFlightCostPerPerson = destination.estimatedFlightCostPerPerson
+    this.image = destination.image
+    this.alt = destination.alt
+  }
 }
 
 export { Destination }
