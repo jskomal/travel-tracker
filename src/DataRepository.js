6be721e531e3810c6b6e3b19b9ff8a5aac4b8266
Jordan Skomal <jskomal@gmail.com>
2022-03-04 13:04:34 -0700

    Add method to calculate total cost

--- src/DataRepository.js
+++ src/DataRepository.js
@@ -4,6 +4,21 @@ class DataRepository {
     this.trips = data.trips
     this.destinations = data.destinations
   }
+
+  calcTotalCost(userID) {
+    const userTrips = this.trips.filter((trip) => trip.userID === userID)
+    const sum = userTrips.reduce((acc, currentTrip) => {
+      let currentDestination = this.destinations.find(
+        (place) => place.id === currentTrip.destinationID
+      )
+      return (
+        acc +
+        currentTrip.duration * currentDestination.estimatedLodgingCostPerDay +
+        currentDestination.estimatedFlightCostPerPerson * currentTrip.travelers
+      )
+    }, 0)
+    return sum
+  }
 }
 
 export { DataRepository }
