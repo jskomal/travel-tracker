6a94bdd76a1d8629617d6c222fcd2e368c5f1a54
Jordan Skomal <jskomal@gmail.com>
2022-03-04 14:42:35 -0700

    Display trips on the DOM

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,4 +1,5 @@
 import { data } from './scripts'
+import dayjs from 'dayjs'
 
 let currentUserID
 // query selectors
@@ -20,8 +21,32 @@ const updateName = () => {
 }
 
 const updateTripView = () => {
-  const renderer = ''
-
+  const currentTrips = data.getTrips(currentUserID - 1, 'all')
+  let renderer = ''
+  currentTrips.forEach((trip) => {
+    let currentDestination = data.destinations.find(
+      (place) => trip.destinationID === place.id
+    )
+    renderer += `
+      <article class='trip-card'>
+        <div class='image-header'>
+          <img
+            src=${currentDestination.image}
+            alt=${currentDestination.alt}
+          />
+          <h3 class='destination-name-text'>${currentDestination.destination}</h3>
+        </div>
+        <section class='trip-info'>
+          <h4>${trip.date.format('MMM D, YY')} to ${trip.date
+      .add(trip.duration, 'days')
+      .format('MMM D, YY')}</h4>
+          <h4>${trip.travelers} travelers</h4>
+          <h4>Suggested Activities: ${trip.suggestedActivites}</h4>
+          <h4>Trip Status: ${trip.status}</h4>
+        </section>
+      </article>
+    `
+  })
   tripView.innerHTML = renderer
 }
 
