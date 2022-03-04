a15a21babfcbabaa53670db44e816970b2d9f27c
Jordan Skomal <jskomal@gmail.com>
2022-03-04 14:08:18 -0700

    Refactor with proper date filtering

--- src/DataRepository.js
+++ src/DataRepository.js
@@ -1,3 +1,5 @@
+import dayjs from 'dayjs'
+
 class DataRepository {
   constructor(data) {
     this.travelers = data.travelers
@@ -5,8 +7,10 @@ class DataRepository {
     this.destinations = data.destinations
   }
 
-  calcTotalCost(userID) {
-    const userTrips = this.trips.filter((trip) => trip.userID === userID)
+  calcTotalCostThisYear(userID) {
+    const userTrips = this.trips.filter((trip) => {
+      return trip.userID === userID && trip.date.isSame(dayjs('1 / 1 / 2022'), 'year')
+    })
     const sum = userTrips.reduce((acc, currentTrip) => {
       let currentDestination = this.destinations.find(
         (place) => place.id === currentTrip.destinationID
@@ -17,7 +21,15 @@ class DataRepository {
         currentDestination.estimatedFlightCostPerPerson * currentTrip.travelers * 2
       )
     }, 0)
-    return sum * 1.1
+    if (!sum) {
+      return `No trips found this year!`
+    } else {
+      return parseFloat((sum * 1.1).toFixed(2))
+    }
+  }
+
+  getTrips(filterTerm) {
+    const userTrips = this.trips.filter((trip) => trip.userID === userID)
   }
 }
 
