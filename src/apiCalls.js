43a7fc30889955a924447cd08bc0ca74e96e2629
Jordan Skomal <jskomal@gmail.com>
2022-03-07 21:17:02 -0700

    Add POST function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,7 +1,37 @@
+import dayjs from 'dayjs'
+import {
+  currentUserID,
+  destinationInput,
+  travelersInput,
+  calendarInput,
+  durationInput
+} from './domUpdates'
+
+let tripIDcounter = 50
+
 const fetchData = (path) => {
   return fetch(`http://localhost:3001/api/v1/${path}`)
     .then((response) => response.json())
     .catch((error) => console.log(error))
+}
+
+const postTrip = () => {
+  return fetch('http://localhost:3001/api/v1/trips', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: {
+      id: tripIDcounter++,
+      userID: currentUserID,
+      destinationID: destinationInput.value,
+      travelers: travelersInput.value,
+      date: dayjs(calendarInput.value).format('YYYY/MM/DD'),
+      duration: durationInput.value,
+      status: 'pending',
+      suggestedActivities: []
+    }
+  })
 }
 
 export { fetchData }
